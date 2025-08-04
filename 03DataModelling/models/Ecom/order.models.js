0021c88828a7c600ebb830357df2f6b8aeb7d846
Arnab Kumar Dey <arnabkumardey9957@gmail.com>
2025-08-05 00:11:32 +0530

    Ecom Website schema

--- 03DataModelling/models/Ecom/order.models.js
+++ 03DataModelling/models/Ecom/order.models.js
@@ -1,8 +1,54 @@
 import mongoose from 'mongoose';
 
+const OrderItemSchma = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "ProductSchema",
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    }
+)
+
+const addressSchema = new mongoose.Schema(
+    {
+        villORCity:{
+            type: String,
+            required: true
+        },
+        landMark:{
+            type: String,
+            required: true
+        },
+        PIN:{
+            type: Number,
+            required: true
+        }
+    }
+)
+
 const orderSchema = new mongoose.Schema(
     {
-        
+        orderPrice: {
+            type:Number,
+            required: true
+        },
+        customer: {
+            type: String,
+            required: true  
+        },
+        orderItems: [OrderItemSchma],
+        address: [addressSchema],
+        status: {
+            type: String,
+            enum: ["PENDING", "CANCLED", "DELIVERED"]
+        }
+
     },
     {timestamps: true}
 )
--- 03DataModelling/models/Ecom/product.models.js
+++ 03DataModelling/models/Ecom/product.models.js
@@ -2,7 +2,37 @@ import mongoose, { mongo } from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-         
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Categories",
+            required: true,
+        },
+        stock: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "UsersSchema"
+        }
+
     },
     {
         timestamps: true
